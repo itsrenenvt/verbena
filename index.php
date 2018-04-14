db91a1f37900a9f0fe5721699c9de79bd4c0226c
itsrenenvt <brnvtrene@gmail.com>
2018-04-14 16:21:38 -0500

    se cambió el index de html a php.

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-widht, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600" rel="stylesheet">  <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="js/jquery-3.3.1.min.js" charset="utf-8"></script>
+    <script src="js/main.js" charset="utf-8"></script>
+    <title>Verbena de la buena</title>
+  </head>
+  <body>
+    <?php
+    include("header_uno.html");
+?>
+    <div class="seccion_1">
+
+    </div>
+
+    <div class="seccion_2">
+
+    </div>
+
+    <div class="seccion_3">
+
+    </div>
+
+    <div class="google-maps">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d60412.89663238948!2d-97.09934193492258!3d18.851290138151015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85c502a4c0439f05%3A0xd83faa836b275780!2sOrizaba%2C+Ver.!5e0!3m2!1ses-419!2smx!4v1523123596789" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+<?php
+    include("footer.html");
+?>
+  </body>
+</html>
