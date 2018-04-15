ceeab1bd77b942ed10af577a41ee4b362d25776d
itsrenenvt <brnvtrene@gmail.com>
2018-04-15 14:35:58 -0500

    se renombró CSS de verbuenos.

--- verbuenos.php
+++ verbuenos.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-widht, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600" rel="stylesheet">  <!-- Google web font "Open Sans" -->
-    <link rel="stylesheet" type="text/css" href="css/team.css">
+    <link rel="stylesheet" type="text/css" href="css/verbuenos.css">
     <link rel="icon" href="img/octocat.ico">
     <script src="js/jquery-3.3.1.min.js" charset="utf-8"></script>
     <script src="js/main.js" charset="utf-8"></script>
@@ -70,7 +70,7 @@
            </div>
 
            <div class="testimonial">
-             <div class="photo-perfil3"></div>
+             <div class="photo-perfil4"></div>
                <h2>Zaira Cerecedo</h2>
                <h4>CEO Verbena</h4>
                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -85,7 +85,7 @@
            </div>
 
            <div class="testimonial">
-             <div class="photo-perfil3"></div>
+             <div class="photo-perfil5"></div>
                <h2>Zaira Cerecedo</h2>
                <h4>CEO Verbena</h4>
                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -100,9 +100,9 @@
            </div>
 
            <div class="testimonial">
-             <div class="photo-perfil3"></div>
+             <div class="photo-perfil6"></div>
                <h2>Zaira Cerecedo</h2>
-               <h4>CEO Verbena</h4>
+               <h4>Coladorador Editorial</h4>
                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 
